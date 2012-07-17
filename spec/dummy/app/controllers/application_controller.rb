15339669793b1ced323598a85dce904769de442d
saberma <mahb45@gmail.com>
2012-07-17 10:58:23 +0800

    use shopqi-app gem and its generator.

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  prepend_before_filter :authenticate_shop!
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
+
+  use_shopqi
+
 end

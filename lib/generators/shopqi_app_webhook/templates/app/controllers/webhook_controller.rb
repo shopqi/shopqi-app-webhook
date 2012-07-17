e1fb654d430c268664a27f30e9a0c8c7bbb01c9e
saberma <mahb45@gmail.com>
2012-07-17 12:23:03 +0800

    pass rspec.

--- lib/generators/shopqi_app_webhook/templates/app/controllers/webhook_controller.rb
+++ lib/generators/shopqi_app_webhook/templates/app/controllers/webhook_controller.rb
@@ -1,33 +1,9 @@
-class WebhookController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  skip_before_filter :authenticate_shop!
-  before_filter :check_source # 检查是否来自 ShopQi
-  before_filter :check_shop
+class WebhookController < ShopQiAppWebhook::WebhookController
 
   def orders_fulfilled
-    request.body.rewind
     attrs = JSON(request.body.read)['order']
-    id = request.headers['HTTP_X_SHOPQI_ORDER_ID']
-    shop.orders.where(order_id: id).first_or_create! name: attrs['name'], order_id: attrs['id'], total_price: attrs['total_price'], created_at: attrs[:created_at]
+    shop.orders.where(order_id: order_id).first_or_create! name: attrs['name'], order_id: attrs['id'], total_price: attrs['total_price'], created_at: attrs['created_at']
     render nothing: true
-  end
-
-  protected
-  def check_source
-    shopqi_sign = request.headers['HTTP_X_SHOPQI_HMAC_SHA256']
-    data = request.body.read
-    digest  = OpenSSL::Digest::Digest.new('sha256')
-    sign = Base64.encode64(OpenSSL::HMAC.digest(digest, SecretSetting.oauth.secret, data)).strip
-    render json: { errors: 'Signature invalid' } and return unless sign == shopqi_sign
-  end
-
-  def check_shop
-    render json: { errors: 'Domain invalid' } and return unless shop
-  end
-
-  def shop
-    domain = request.headers['HTTP_X_SHOPQI_DOMAIN']
-    @shop ||= Shop.find_by_shopqi_domain(domain) unless domain.blank?
   end
 
 end
--- lib/generators/shopqi_app_webhook/templates/db/migrate/create_orders.rb
+++ lib/generators/shopqi_app_webhook/templates/db/migrate/create_orders.rb
@@ -5,7 +5,7 @@ class CreateOrders < ActiveRecord::Migration
       t.integer :order_id   , unique: true, null: false
       t.string :name        , limit: 32   , null: false
       t.float :total_price  , null: false
-      t.datetime :created_at
+      t.datetime :created_at, null: false
     end
     add_index :orders, :shop_id, unique: true
   end

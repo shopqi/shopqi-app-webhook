280b1ddd201e73d63f4eb0bc32879830fab071e4
saberma <mahb45@gmail.com>
2012-08-07 10:37:32 +0800

    use shop admin_url.

--- spec/dummy/app/models/order.rb
+++ spec/dummy/app/models/order.rb
@@ -3,6 +3,6 @@ class Order < ActiveRecord::Base
   attr_accessible :name, :financial_status, :financial_status_name, :fulfillment_status, :fulfillment_status_name, :order_id, :total_price, :created_at
 
   def admin_url
-    "#{SecretSetting.shopqi_domain.protocol}#{shop.shopqi_domain}/admin/orders/#{order_id}"
+    "#{shop.admin_url}/orders/#{order_id}"
   end
 end
